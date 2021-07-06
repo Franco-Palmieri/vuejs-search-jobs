92734629875326827b17d466711c2d0dabf066ff
Ncesecrede <francescopm95@gmail.com>
2021-07-06 10:17:34 +0100

    Web App Search Jobs

---
+++ script.js
@@ -0,0 +1,63 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },  
+                {
+                    id: 2,
+                    company: 'Azienda Due',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Milano',
+                    contract: 'Full Time'
+                },  
+                {
+                    id: 3,
+                    company: 'Azienda 3',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Genova',
+                    contract: 'Full Time'
+                }, 
+                {
+                    id: 4,
+                    company: 'Azienda 4',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Frosinone',
+                    contract: 'Full Time'
+                }, 
+                {
+                    id: 5,
+                    company: 'Azienda 5',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Torino',
+                    contract: 'Full Time'
+                }, 
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]
+        },
+        methods: {
+        },
+    }
+);
